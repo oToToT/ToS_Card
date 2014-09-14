cf28095c2527789cff8a2a0425589862065319e5
oToToT <ty1208chiang@gmail.com>
2014-09-14 11:26:42 +0800

    ttttt

---
+++ test/test.js
@@ -0,0 +1,68 @@
+	(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	window.fbAsyncInit = function() {
+	    FB.init({
+	    	appId: "791606320879048",
+	    	status: true, 
+	    	cookie: true, 
+	    	xfbml: true, 
+	    	oauth: true, 
+	    	version: 'v2.0'
+	    	}
+		);
+	}
+	
+	function shareToFB () {
+		FB.getLoginStatus(function(response) {
+		    if (response.status === 'connected') {
+		        //已登入且和app連結
+		        var uid = response.authResponse.accessToken;
+		        accessToken = response.authResponse.accessToken;
+		        init();
+		    } else if (response.status === 'not_authorized') {
+		        //已登入但未與app連結
+		        login();
+		    } else {
+		        //未登入
+		        login()
+		    }
+		});
+	}
+	function login() {
+	    FB.login(function(response) {
+	        if (response.authResponse) {
+	            init()
+	        }else{
+	        	$("#load").modal("hide")
+				get("url").innerHTML = "登入失敗"
+				$("#sdone").modal("show")
+	        }
+	    }, { scope: 'public_profile,publish_actions' });
+	}
+	function init () {
+		var link = ""
+		var imgURL = ""
+		function(req) {
+	    	FB.api('/me/feed', 'post', {
+	    		access_token: accessToken,
+		    	message: get("pd").value,
+				}, function(response){
+				    if (!response || response.error) {
+				    	$("#load").modal("hide")
+				        get("url").innerHTML = "分享錯誤"
+				        $("#sdone").modal("show")
+				    } else {
+				    	$("#load").modal("hide")
+				        get("url").innerHTML = "https://www.facebook.com/photo.php?fbid="+response.id.toString()
+				        get("url").href = get("url").innerHTML
+				        $("#sdone").modal("show")
+				    }
+
+				});
+			}
+	}
